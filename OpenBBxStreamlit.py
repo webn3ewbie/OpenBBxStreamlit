6022bd38e8b7a420325ee2cae616f586b7776f8a
webn3ewbie <96390676+webn3ewbie@users.noreply.github.com>
2022-12-07 14:20:30 -0500

    Update OpenBBxStreamlit.py

--- OpenBBxStreamlit.py
+++ OpenBBxStreamlit.py
@@ -25,7 +25,9 @@ with col4:
 
 
 with st.container():
-    st.sidebar.write('Special thanks to the team at [Openbb](https://openbb.co/) Excited to see what the future holds as the lead the way in open source investment research!')
+    st.sidebar.write('Special thanks to the team at [Openbb](https://openbb.co/) Excited to see what the future holds as the lead the way in open source investment research!'
+                     'Feel free to reach [out](https://twitter.com/DirtyDefi) I love talking anything markets and programming. '
+                     'Please note this app is NOT financial advice. The dashboards is NOT intended to help guide financial decisions!')
     st.sidebar.write('')
     
 
