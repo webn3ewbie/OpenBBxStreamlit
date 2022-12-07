67e1557d66310735446cbdce3477181b98502a68
webn3ewbie <96390676+webn3ewbie@users.noreply.github.com>
2022-12-07 14:28:01 -0500

    Update OpenBBxStreamlit.py

--- OpenBBxStreamlit.py
+++ OpenBBxStreamlit.py
@@ -25,7 +25,7 @@ with col4:
 
 
 with st.container():
-    st.sidebar.write('Special thanks to the team at [Openbb](https://openbb.co/) Excited to see what the future holds as the lead the way in open source investment research!'
+    st.sidebar.write('Special thanks to the team at [Openbb](https://openbb.co/) Excited to see what the future holds as the lead the way in open source investment research!')
                      
     st.sidebar.write('Feel free to reach [out](https://twitter.com/DirtyDefi) I love talking anything markets and programming.')
                      
