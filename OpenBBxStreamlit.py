f5830551576d77fcdd36209f1c674d1b908530b7
webn3ewbie <96390676+webn3ewbie@users.noreply.github.com>
2022-12-12 19:29:09 -0500

    Update OpenBBxStreamlit.py

--- OpenBBxStreamlit.py
+++ OpenBBxStreamlit.py
@@ -152,7 +152,7 @@ with col1:
     data['mcap_change_24h'] = data['mcap_change_24h' ].apply(pd.to_numeric)
     data['pct_change_1h'] = data['pct_change_1h' ].apply(pd.to_numeric)
     data['pct_from_ath'] = data['pct_from_ath' ].apply(pd.to_numeric)
-    st.dataframe(data.style.applymap(color_negative_red, subset=['pct_change_24h','mcap_change_24h','pct_change_1h','pct_from_ath'],limit=1000))  
+    st.dataframe(data.style.applymap(color_negative_red, subset=['pct_change_24h','mcap_change_24h','pct_change_1h','pct_from_ath']))  
 with col2:
     st.subheader('Crypto Hacks')
     st.dataframe(openbb.crypto.ov.crypto_hacks())
