de5c609a98a2b0f057f70b0773c5ed9c361cc551
Joseph B <96390676+webn3ewbie@users.noreply.github.com>
2023-02-19 15:59:56 -0500

    Update OpenBBxStreamlit.py

--- OpenBBxStreamlit.py
+++ OpenBBxStreamlit.py
@@ -93,7 +93,7 @@ with col2:
 st.title('Economy')
 col1,col2=st.columns([55,55]) 
 with col1:
-    st.pyplot(openbb.economy.inf_chart()) 
+    st.dataframe(openbb.economy.events()) 
 with col2:
     st.pyplot(openbb.economy.cpi_chart())   
 col1,col2=st.columns([55,55]) 
