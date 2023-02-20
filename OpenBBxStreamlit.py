dd9ef4b9c3b9918f72e19a5346585d8bbf3b79de
Joseph B <96390676+webn3ewbie@users.noreply.github.com>
2023-02-20 12:28:58 -0500

    Update OpenBBxStreamlit.py

--- OpenBBxStreamlit.py
+++ OpenBBxStreamlit.py
@@ -95,7 +95,7 @@ col1,col2=st.columns([55,55])
 with col1:
     st.pyplot(openbb.economy.fred_chart('CPIAUCSL')) 
 with col2:
-    st.pyplot(openbb.economy.macro_chart())   
+    st.pyplot(openbb.economy.fred_chart('FPCPITOTLZGUSA'))   
 col1,col2=st.columns([55,55]) 
 with col1:
     st.pyplot(openbb.economy.gdp_chart()) 
